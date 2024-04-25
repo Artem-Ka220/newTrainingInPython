0428e4e129bb6b302007babe143993a45c99b36a
arvsokolov <marktwe@mail.ru>
2024-04-25 10:11:06 +0300

    New Homework.

---
+++ lesson_2/task_1.py
@@ -0,0 +1,33 @@
+"""
+Задача №9. Решение в группах
+По данному целому неотрицательному n вычислите
+значение n!. N! = 1 * 2 * 3 * … * N (произведение всех
+чисел от 1 до N) 0! = 1 Решить задачу используя цикл
+while
+Input: 5
+Output: 120
+"""
+num = 5
+f = 1
+
+while num > 1:
+    f *= num
+    num -= 1
+print(f)
+
+for el in range(1, num + 1):
+    f *= el
+print(f)
+
+def factorial(num, f=1):
+    if num == 0:
+        return f
+    return factorial(num - 1, f*num)
+
+print(factorial(5))
+
+num = [5, 5.2, True, 'Artem']
+print(num)
+
+nums = (2, 3, 3.2, 'Sokolov', False, True, 4.15)
+print(nums)
